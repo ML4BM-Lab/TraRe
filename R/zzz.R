b88d43a8de9222799f4a4c1934e8d5a3588ee824
Jesus de la Fuente Cedeno <jdelafuentec@unav.es>
2020-11-25 14:31:23 +0100

    Bioccheck 0 errors and 0 warnings

--- R/zzz.R
+++ R/zzz.R
@@ -1,16 +1,6 @@
 #' Transcriptional Rewiring package for R.
 #'
-#' @section Description of TraRe:
-#'
-#' *TraRe* (Transcriptional Rewiring) is an R package which contains the necessary tools
-#' to carry out: Identification of module-based gene regulatory networks (GRN); score-based
-#' classification of these modules via a rewiring test; visualization of rewired modules
-#' to analyze condition-based GRN deregulation and drop out genes recovering via cliques
-#' methodology. For each tool, html report can be generated containing useful information
-#' about the generated GRN and statistical data about the performed tests. This tools have
-#' been developed considering sequenced data (RNA-Seq).
-#'
-#' *TraRe* offers the following tools
+#' @section Available tools:
 #'
 #' \itemize{
 #'
