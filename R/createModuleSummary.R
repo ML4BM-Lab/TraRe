4759dc8860d1455a7ffa850b6cac1c5294a934c3
Zikun Chen <zikunchen@Zikuns-MacBook-Pro.local>
2021-02-19 02:02:12 -0600

    minor clean up

--- R/createModuleSummary.R
+++ R/createModuleSummary.R
@@ -1,5 +1,4 @@
 createModuleSummary <- function(ObjectList,modmeth="VBSR", numclus=1, supertype="refined") {
-	# modmeths <- names(ObjectList$datasets[[1]]$rundata$modules)
 	alllabels = ObjectList$datasets[[1]]$responder[ObjectList$datasets[[1]]$keepsamps]
 	samps2pheno = alllabels
 	samps2pheno[which(alllabels == ObjectList$phenotype_class_vals_label[2])] = ObjectList$phenotype_class_vals[2]
@@ -9,16 +8,16 @@ createModuleSummary <- function(ObjectList,modmeth="VBSR", numclus=1, supertype=
 	responder_idxs = names(samps2pheno)[which(samps2pheno == ObjectList$phenotype_class_vals[2])]
 
 	modsumm_name = paste0(ObjectList$outdir,"/supermodule_",numclus,"/",supertype,"summ.rds")
-	if (file.exists(modsumm_name) && file.exists(runmoddata_name)){
+	if (file.exists(modsumm_name)){
 		modsumm <- readRDS(modsumm_name)
 	} else {
-        stop("Rewiring files does not exist, please run runrewiring before creating module summary.")
+        stop("Rewiring file does not exist, please run runrewiring() before creating module summary.")
 	}
 
 	# set up output html page
 	codedir <- paste0(system.file("extdata",package="TraRe"),"/RewiringReport/")
-	dir.create(file.path(paste0(ObjectList$outdir,"/mod_summ_html/")), showWarnings = FALSE)
-	htmlinfo <- create_index_page(outdir = paste0(ObjectList$outdir,"/mod_summ_html"), runtag = "", codedir = codedir)
+	dir.create(file.path(paste0(ObjectList$outdir,"/module_summ.",modmeth,".",numclus,".",supertype,"/")), showWarnings = FALSE)
+	htmlinfo <- create_index_page(outdir = paste0(ObjectList$outdir,"/module_summ.",modmeth,".",numclus,".",supertype), runtag = "", codedir = codedir)
 
 	orderobj <- geneOrder(modsumm, ObjectList$datasets[[1]]$keepsamps, ObjectList$datasets[[1]]$keeplabels, ObjectList$datasets[[1]]$norm_expr_mat_keep)
 	createLegendPlot(htmlinfo)
@@ -28,10 +27,8 @@ createModuleSummary <- function(ObjectList,modmeth="VBSR", numclus=1, supertype=
 	correlationOfModuleGene(supertype, orderobj$regorder, orderobj$targetorder, orderobj$mat, orderobj$cormats, ObjectList$keeplabels, htmlinfo, ObjectList$phenotype_class_vals)
 	expressionTableOfModuleGenes(supertype, modsumm$nodesumm, htmlinfo)
 	expressionPlotsOfModuleGenes(supertype, orderobj$regorder, orderobj$targetorder, orderobj$mat, samps2pheno, ObjectList$phenotype_class_vals, htmlinfo)
-	# bipartiteGraphsSumm(numclus, modsumm, ObjectList$datasets[[1]]$norm_expr_mat_keep, nonrespond_idxs, responder_idxs, modmeth, htmlinfo)
-	bipartiteGraphsSumm(numclus, modsumm, modmeth, htmlinfo)
+	bipartiteGraphsSumm(numclus, modsumm, modmeth, htmlinfo,ObjectList$outdir)
 	nullDistributionOfRewiringStatistic(orderobj$mat, ObjectList$datasets[[1]]$keeplabels, modmeth, supertype, htmlinfo)
 	rankdf <- violinPlots(ObjectList$datasets[[1]]$norm_expr_mat_keep, ObjectList$datasets[[1]]$keepsamps, ObjectList$datasets[[1]]$keeplabels, modsumm$nodesumm, modsumm$fulledgesumm, modsumm$appendmat, htmlinfo)
 	regulatorSummaryAndRank(rankdf, htmlinfo)
 }
-
